57b337b55f446a14e744eaeb7234f4dd7279fbcb
Wzcjoyce <93357863+Wzcjoyce@users.noreply.github.com>
2022-10-13 00:42:34 -0400

    tuiter half

---
+++ public/tuiter/NavigationSideBar/index.js
@@ -0,0 +1,170 @@
+const NavigationSidebar = () => {
+    return(`
+            <div class = "text-padding-top-20 menu-left-padding">
+                <i class="fa-sharp fa-solid fa-t fa-2x"></i>
+            </div>
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-home fa-2x col-1"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Home</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa <i fa-solid fa-hashtag fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Explore</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-bell fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Notification</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div  class = "col-1">
+                    <i class="fa fa-envelope fa-2x"></i>
+                </div>
+                <div  class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Messages</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-bookmark fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Bookmarks</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div  class = "col-1">
+                    <i class="fa fa-list fa-2x"></i>
+                </div>
+                <div  class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">List</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-user fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Profile</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-comment-dots fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">More</span>
+                </div>
+            </div>
+
+            <div class="text-padding-top-40 menu-left-padding col-12 col-sm-12 col-md-12  col-lg-12 col-xl-12 col-xxl-12">
+                <a href="#" class="btn btn-width btn-primary rounded-pill "><span class = "font-12">Tuiter</span></a>
+            </div>
+
+ `);
+}
+export default NavigationSidebar;
+
+
+function NavigationSidebarComponent() {
+    $('#wd-NavigationSidebar').append(`
+            <div class = "text-padding-top-20 menu-left-padding">
+                <i class="fa-sharp fa-solid fa-t fa-2x"></i>
+            </div>
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-home fa-2x col-1"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Home</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa <i fa-solid fa-hashtag fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Explore</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-bell fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Notification</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div  class = "col-1">
+                    <i class="fa fa-envelope fa-2x"></i>
+                </div>
+                <div  class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Messages</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-bookmark fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Bookmarks</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div  class = "col-1">
+                    <i class="fa fa-list fa-2x"></i>
+                </div>
+                <div  class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">List</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-user fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">Profile</span>
+                </div>
+            </div>
+
+            <div class = "text-padding-top-40 row menu-left-padding">
+                <div class = "col-1">
+                    <i class="fa fa-comment-dots fa-2x"></i>
+                </div>
+                <div class = "col-1">
+                    <span class = "text-padding-left-10 d-none d-sm-none d-md-none d-lg-none d-xl-block d-xxl-block fa-1x">More</span>
+                </div>
+            </div>
+
+            <div class="text-padding-top-40 menu-left-padding col-12 col-sm-12 col-md-12  col-lg-12 col-xl-12 col-xxl-12">
+                <a href="#" class="btn btn-width btn-primary rounded-pill "><span class = "font-12">Tuiter</span></a>
+            </div>
+   `);
+}
+
+$(NavigationSidebarComponent);
---
+++ public/tuiter/WhoToFollowList/WhoToFollowListItem.js
@@ -0,0 +1,25 @@
+const WhoToFollowListItem = (whos) => {
+    return(`
+            <div class = " padding-all-15 col gray_bg">
+                <div class = "row">
+                    <div class = "col-3 col-lg-2 col-xl-2 col-xxl-2">
+                        <img class=" user-image" src=${whos.avatarIcon}>
+                    </div>
+                    <div class = "col-5 col-lg-6 col-xl-6 col-xxl-6">
+                        <div class = "fond-size-10">
+                            ${whos.userName}
+                            <i class="fa fa-check"></i>
+                        </div>
+                        <div class = "text-gray fond-size-10">
+                            ${whos.Handle}
+                        </div>
+
+                    </div>
+                    <div class = "col-3 col-lg-4 col-xl-4 col-xxl-3">
+                        <a href="#" class="btn btn-width2  btn-primary rounded-pill"><div class = "font-size-12">Follow</div></a>
+                    </div>
+                </div>
+            </div>
+ `);
+}
+export default WhoToFollowListItem;
