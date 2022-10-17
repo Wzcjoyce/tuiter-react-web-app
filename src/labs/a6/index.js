d44926868d8d0ab0f0856f273fe7534f432cb26b
Wzcjoyce <93357863+Wzcjoyce@users.noreply.github.com>
2022-10-16 21:55:36 -0400

    a6 2.11

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,7 +1,15 @@
+import {Link}
+    from "react-router-dom";
+import Nav from "../../nav.js";
+import Classes from "./classes";
+import Styles from "./styles";
+
 function Assignment6() {
     return (
         <div>
             <h1>Assignment 6</h1>
+            <Styles/>
+            <Classes/>
         </div>
     );
 }
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,8 +1,10 @@
 import Assignment6 from "./a6";
+import Nav from "../nav.js";
 
 function Labs() {
     return(
         <div>
+            <Nav/>
             <Assignment6/>
         </div>
     );
