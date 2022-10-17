ec0fe7c9c4fb71b5eae61fd10cdd60b47391f6fc
Wzcjoyce <93357863+Wzcjoyce@users.noreply.github.com>
2022-10-16 20:01:22 -0400

    a6_2.5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          CS5610 Fall 2022 Zuocheng Wang a1
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.ordg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <HelloWorld/>
+        <Labs/>
+      </div>
   );
 }
-
 export default App;
